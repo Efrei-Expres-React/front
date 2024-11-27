5c63a4e5fc27a585d8197e674e806070ac680411
Marie <mje.espinosa@gmail.com>
2024-11-27 15:00:45 +0100

    recupere mes cv et recos

--- src/pages/MyCV.jsx
+++ src/pages/MyCV.jsx
@@ -1,50 +1,77 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import './MyCV.css';  // Importer le fichier CSS pour le style de la carte
+import React, { useRef } from 'react'
+import { useState, useCallback, useEffect } from 'react';
+import { getMyCV, deleteCV as deleteOneCV} from '../api/cv';
+import { useContext } from 'react';
+import { AuthContext } from '../utils/context/AuthContext';
+import CVlist from '../components/layout/CVlist';
+import RecoAccordion from '../components/layout/RecoAccordion';
+import { deleteRecommandation, getMyrecomandations } from '../api/recomandations';
+import Alerts from '../components/atoms/Alerts';
 
-const MyCV = () => {
-  const [cvData, setCvData] = useState(null);
+
+const MyCvs = () => {
+const [cvs, setCvs] = useState();
+const [recommandations, setRecommandations] = useState();
+const isMounted = useRef(false)
+const {token } = useContext(AuthContext);
+const [error, setError] = useState();
+
+
+const getProfile = useCallback( async () => {
+    try {
+        const res =  await getMyCV(token);
+        setCvs(res.data.cv)
+    } catch (error) {
+        setError(error.message)
+    }
+  }, [token]);
+
+  const getRecommandations = useCallback( async () => {
+    try {
+        const res =  await getMyrecomandations(token);
+        setRecommandations(res.data.message)
+    } catch (error) {
+        setError(error.message)
+    }
+  }, [token]);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('URL_TO_YOUR_API'); // Remplace par l'URL de ton API
-        setCvData(response.data); // Assurez-vous que 'response.data' a la structure correcte
-      } catch (error) {
-        console.error('Erreur de récupération des données:', error);
-      }
-    };
-    fetchData();
-  }, []);
+    if(!isMounted.current && token) {
+        getProfile()
+        getRecommandations()
+        isMounted.current = false
+    }
+  }, [isMounted, token]);
 
-  if (!cvData) {
-    return <div>Chargement...</div>; // Affiche un message de chargement si les données sont encore à récupérer
+  const deleteCV = async (id) =>{
+    try {
+      await deleteOneCV(id, token)
+      await getProfile(token)
+      await getRecommandations()
+    } catch (error) {
+      setError(error.message)
+    }
   }
 
-  // Assurez-vous que 'cvData.experiences' est un tableau avant de faire un map
-  const experiences = Array.isArray(cvData.experiences) ? cvData.experiences : [];
+  const deleteRecommandations = async (id) =>{
+    try {
+      await deleteRecommandation(id, token)
+      await getRecommandations()
+    } catch (error) {
+      setError(error.message)
+    }
+  }
+
 
   return (
-    <div className="my-cv-card">
-      <h2>{cvData.firstname} {cvData.lastname}</h2>
-      <p>{cvData.description}</p>
-      <div className="experiences">
-        {experiences.length > 0 ? (
-          experiences.map((experience, index) => (
-            <div key={index} className="experience">
-              <h3>{experience.title}</h3>
-              <p>{experience.company}</p>
-              <p>{experience.startDate} - {experience.endDate}</p>
-              <p>{experience.description}</p>
-            </div>
-          ))
-        ) : (
-          <p>Aucune expérience disponible.</p>
-        )}
-      </div>
-    </div>
-  );
-};
+    <>
+     {error && <Alerts message={error} type='error'/> }
 
-export default MyCV;
+      <CVlist showButtons={true} cvs={cvs} deleteCV={deleteCV} title={"Mes CV"}/>
 
+      <RecoAccordion recoList={recommandations} onDelete={deleteRecommandations}/>
+    </>
+  )
+}
+
+export default MyCvs
